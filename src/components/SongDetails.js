28bcb115b758c74465c63bf1707ec8bb98bec2b8
ange47rm <ange47rm@hotmail.com>
2021-03-02 17:29:42 +0000

    Created file structure and components

---
+++ src/components/SongDetails.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const SongDetails = () => {
+
+    return (
+        <>
+            <h3>Song Details</h3>
+        </>
+    );
+}
+
+export default SongDetails;
---
+++ src/containers/MusicChartContainer.js
@@ -0,0 +1,14 @@
+import React from "react"
+import SongDetails from "../components/SongDetails"
+
+const MusicChartContainer = () => {
+
+    return (
+        <>
+            <h3>Music Chart Container</h3>
+            <SongDetails/>
+        </>
+    );
+}
+
+export default MusicChartContainer;

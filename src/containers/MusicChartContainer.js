57886e1e39701cff84a260151d9b0e50b5cbcd73
ange47rm <ange47rm@hotmail.com>
2021-03-02 17:52:01 +0000

    API information received

--- src/containers/MusicChartContainer.js
+++ src/containers/MusicChartContainer.js
@@ -1,11 +1,26 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import SongDetails from "../components/SongDetails"
 
 const MusicChartContainer = () => {
 
+    const [songs, setSongs] =  useState([]);
+
+    const getSongs = () => {
+        console.log("Getting songs information");
+        fetch ("https://itunes.apple.com/gb/rss/topsongs/limit=20/json")
+        .then (response => response.json())
+        .then (dataFromAPI => setSongs(dataFromAPI.feed.entry[0]))
+    }
+
+    useEffect(() => {
+        console.log ("Use effect triggered")
+        getSongs();},[])                        // the empty list prevents getSongs being called infinitely
+    
+
+
     return (
         <>
-            <h3>Music Chart Container</h3>
+            <h3>Music Chart</h3>
             <SongDetails/>
         </>
     );
